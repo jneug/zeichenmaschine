5b6584690924cce1038db854dc7ebceada7b546a
J. Neugebauer <ngb@helmholtz-bi.de>
2022-01-05 17:21:12 +0100

    Formatierung angepasst

--- examples/zm_tixy/TIXY.java
+++ examples/zm_tixy/TIXY.java
@@ -1,7 +1,7 @@
 import schule.ngb.zm.DrawableLayer;
 import schule.ngb.zm.Zeichenmaschine;
 
-public class TIXY  extends Zeichenmaschine {
+public class TIXY extends Zeichenmaschine {
 
 	public static final int FIELD_SIZE = 16;
 
@@ -9,6 +9,9 @@ public class TIXY  extends Zeichenmaschine {
 
 	public static final int DOT_GAP = 4;
 
+	public static void main( String[] args ) {
+		new TIXY();
+	}
 
 	private Dot[][] dots;
 
@@ -34,7 +37,7 @@ public class TIXY  extends Zeichenmaschine {
 		dots = new Dot[FIELD_SIZE][FIELD_SIZE];
 		for( int i = 0; i < FIELD_SIZE; i++ ) {
 			for( int j = 0; j < FIELD_SIZE; j++ ) {
-				dots[i][j] = new Dot(i, j, i*FIELD_SIZE + j);
+				dots[i][j] = new Dot(i, j, i * FIELD_SIZE + j);
 				dl.add(dots[i][j]);
 			}
 		}
@@ -49,16 +52,12 @@ public class TIXY  extends Zeichenmaschine {
 		for( int i = 0; i < FIELD_SIZE; i++ ) {
 			for( int j = 0; j < FIELD_SIZE; j++ ) {
 				Dot d = dots[i][j];
-				double value = tixy.update(runtime/1000.0, d.i, d.x, d.y);
+				double value = tixy.update(runtime / 1000.0, d.i, d.x, d.y);
 				dots[i][j].setValue(value);
 
-				tixy.update(runtime/1000.0, dots[i][j]);
+				tixy.update(runtime / 1000.0, dots[i][j]);
 			}
 		}
-	}
-
-	public static void main( String[] args ) {
-		new TIXY();
 	}
 
 }
