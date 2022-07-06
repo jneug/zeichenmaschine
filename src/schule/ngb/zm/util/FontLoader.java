aadec6d973612f724dec018235ab453bd22951e5
J. Neugebauer <ngb@helmholtz-bi.de>
2022-07-06 20:29:21 +0200

    Refactoring des FontLoader
    Das Öffnen von Resourcen Streams wurde in eine eigene Hilfsklasse ausgelagert. FontLoader nutzt nun die neue Logging API.

--- src/schule/ngb/zm/util/FontLoader.java
+++ src/schule/ngb/zm/util/FontLoader.java
@@ -2,13 +2,10 @@ package schule.ngb.zm.util;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FontLoader {
@@ -16,10 +13,13 @@ public class FontLoader {
 	private static final Map<String, Font> fontCache = new ConcurrentHashMap<>();
 
 	public static Font loadFont( String source ) {
-		if( source == null || source.length() == 0 )
-			throw new IllegalArgumentException("Font source may not be null or empty.");
+		Objects.requireNonNull(source, "Font source may not be null");
+		if( source.length() == 0 ) {
+			throw new IllegalArgumentException("Font source may not be empty.");
+		}
 
 		if( fontCache.containsKey(source) ) {
+			LOG.trace("Retrieved font <%s> from font cache.", source);
 			return fontCache.get(source);
 		}
 
@@ -27,47 +27,33 @@ public class FontLoader {
 		Font font = Font.decode(source);
 		if( font != null && source.toLowerCase().contains(font.getFamily().toLowerCase()) ) {
 			fontCache.put(source, font);
+			LOG.debug("Loaded system font for <%s>.", source);
 			return font;
+		} else {
+			font = null;
 		}
 
 		// Load userfonts
-		try {
-			InputStream in;
-
-			File file = new File(source);
-			if( file.isFile() ) {
-				in = new FileInputStream(file);
-			} else {
-				// load ressource relative to .class-file
-				in = FontLoader.class.getResourceAsStream(source);
-
-				// relative to ClassLoader
-				if( in == null ) {
-					in = FontLoader.class.getClassLoader().getResourceAsStream(source);
-				}
-
-				// load form web or jar-file
-				if( in == null ) {
-					in = new URL(source).openStream();
-				}
-
-
-				font = Font.createFont(Font.TRUETYPE_FONT, in).deriveFont(Font.PLAIN);
-			}
+		try( InputStream in = ResourceStreamProvider.getResourceStream(source) ) {
+			font = Font.createFont(Font.TRUETYPE_FONT, in).deriveFont(Font.PLAIN);
 
 			if( font != null ) {
 				fontCache.put(source, font);
 				//ge.registerFont(font);
 			}
-
-			return font;
-		} catch( IOException | FontFormatException ex ) {
-			// TODO: Log error!
-			return null;
+			LOG.debug("Loaded custom font from <%s>.", source);
+		} catch( IOException ioex ) {
+			LOG.error(ioex, "Error loading custom font file from source <%s>.", source);
+		} catch( FontFormatException ffex ) {
+			LOG.error(ffex, "Error creating custom font from source <%s>.", source);
 		}
+
+		return font;
 	}
 
 	private FontLoader() {
 	}
+
+	private static final Log LOG = Log.getLogger(FontLoader.class);
 
 }
