49063c9581be3b850a6e4fc4de86713eb2c33dd4
J. Neugebauer <ngb@helmholtz-bi.de>
2022-07-06 20:16:37 +0200

    Klasse Validator eingefügt

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,11 +6,14 @@ und diese Projekt folgt [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ## [Unreleased]
 
+## Version 0.0.21
+
 ### Added
 - Parameter `stop_after_draw` im Konstruktor der `Zeichenmaschine` erlaubt es beim Erstellen festzulegen, ob nach dem ersten Frame die Zeichenmaschine gestoppt wird.
 - `Picture.tint(Color)` färbt ein Bild ein.
 - `Picture.flip(Options.Direction)` spiegelt ein Bild entlang einer Achse (`LEFT`/`RIGHT` für horizontal, `UP`/`DOWN` für vertikal).
-- Abstrakte Klasse `Zeichenobjekt` als einheitliche Oberklasse für Objekte in Projekten. Die Klasse erbt von `Constants` und implementiert `Drawabale` und `Updatable` mit leeren Methoden.  
+- Abstrakte Klasse `Zeichenobjekt` als einheitliche Oberklasse für Objekte in Projekten. Die Klasse erbt von `Constants` und implementiert `Drawabale` und `Updatable` mit leeren Methoden.
+- Klasse `java.util.Validator` übernimmt intern Parametervalidierung.
 
 ### Changed
 - Objektvariablen der `Zeichenmaschine`, die von Unterklassen genutzt werden sollen, sind nun statisch in `Constants`. Dadurch können auch andere Klasse, die von `Constants` erben ohne Umwege auf diese Werte zugreifen (z.B. `width`/`height` der Zeichenleinwand).  
---
+++ src/schule/ngb/zm/util/Validator.java
@@ -0,0 +1,152 @@
+package schule.ngb.zm.util;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Validator {
+
+	public static final <T> T requireNotNull( T obj ) {
+		return Objects.requireNonNull(obj);
+	}
+
+	public static final <T> T requireNotNull( T obj, CharSequence msg ) {
+		return Objects.requireNonNull(obj, msg::toString);
+	}
+
+	public static final <T> T requireNotNull( T obj, Supplier<String> msg ) {
+		return Objects.requireNonNull(obj, msg);
+	}
+
+	public static final String requireNotEmpty( String str ) {
+		return requireNotEmpty(str, (Supplier<String>)null);
+	}
+
+	public static final String requireNotEmpty( String str, CharSequence msg ) {
+		return requireNotEmpty(str, msg::toString);
+	}
+
+	public static final String requireNotEmpty( String str, Supplier<String> msg ) {
+		if( str.isEmpty() )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter may not be empty string (<%s> provided)", str) : msg.get());
+		return str;
+	}
+
+	public static final int requirePositive( int i ) {
+		return requirePositive(i, (Supplier<String>)null);
+	}
+
+	public static final int requirePositive( int i, CharSequence msg ) {
+		return requirePositive(i, msg::toString);
+	}
+
+	public static final int requirePositive( int i, Supplier<String> msg ) {
+		if( i <= 0 )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter must be positive (<%d> provided)", i) : msg.get());
+		return i;
+	}
+
+	public static final int requireNotNegative( int i ) {
+		return requireNotNegative(i, (Supplier<String>)null);
+	}
+
+	public static final int requireNotNegative( int i, CharSequence msg ) {
+		return requireNotNegative(i, msg::toString);
+	}
+
+	public static final int requireNotNegative( int i, Supplier<String> msg ) {
+		if( i < 0 )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter may not be negative (<%d> provided)", i) : msg.get());
+		return i;
+	}
+
+	public static final int requireInRange( int i, int min, int max ) {
+		return requireInRange(i, min, max, (Supplier<String>)null);
+	}
+
+	public static final int requireInRange( int i, int min, int max, CharSequence msg ) {
+		return requireInRange(i, min, max, msg::toString);
+	}
+
+	public static final int requireInRange( int i, int min, int max, Supplier<String> msg ) {
+		if( i < min || i > max )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter out of range <%d> to <%d> (<%d> provided)", min, max, i) : msg.get());
+		return i;
+	}
+
+	public static final double requirePositive( double i ) {
+		return requirePositive(i, (Supplier<String>)null);
+	}
+
+	public static final double requirePositive( double i, CharSequence msg ) {
+		return requirePositive(i, msg::toString);
+	}
+
+	public static final double requirePositive( double i, Supplier<String> msg ) {
+		if( i <= 0 )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter must be positive (<%f> provided)", i) : msg.get());
+		return i;
+	}
+
+	public static final double requireNotNegative( double i ) {
+		return requireNotNegative(i, (Supplier<String>)null);
+	}
+
+	public static final double requireNotNegative( double i, CharSequence msg ) {
+		return requireNotNegative(i, msg::toString);
+	}
+
+	public static final double requireNotNegative( double i, Supplier<String> msg ) {
+		if( i < 0 )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter may not be negative (<%f> provided)", i) : msg.get());
+		return i;
+	}
+
+	public static final double requireInRange( double i, double min, double max ) {
+		return requireInRange(i, min, max, (Supplier<String>)null);
+	}
+
+	public static final double requireInRange( double i, double min, double max, CharSequence msg ) {
+		return requireInRange(i, min, max, msg::toString);
+	}
+
+	public static final double requireInRange( double i, double min, double max, Supplier<String> msg ) {
+		if( i < min || i > max )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter out of range <%f> to <%f> (<%f> provided)", min, max, i) : msg.get());
+		return i;
+	}
+
+	/*
+	public static final <N extends Number> N requirePositive( N num, Supplier<String> msg ) {
+		if( num.doubleValue() <= 0 )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter must be positive (<%s> provided)", num.toString()) : msg.get());
+		return num;
+	}
+
+	public static final <N extends Number> N requireNotNegative( N num, Supplier<String> msg ) {
+		if( num.doubleValue() < 0 )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter may not be negative (<%s> provided)", num.toString()) : msg.get());
+		return num;
+	}
+
+	public static final <N extends Number> N requireInRange( N num, N min, N max, Supplier<String> msg ) {
+		if( num.doubleValue() < min.doubleValue() || num.doubleValue() > max.doubleValue() )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter out of range <%s> to <%s> (<%s> provided)", min.toString(), max.toString(), num.toString()) : msg.get());
+		return num;
+	}
+	*/
+
+	public static final <T> T[] requireNotEmpty( T[] arr, Supplier<String> msg ) {
+		return requireSize(arr, 0, ()->"Parameter array may not be empty");
+	}
+
+	public static final <T> T[] requireSize( T[] arr, int size, Supplier<String> msg ) {
+		if( arr.length != size )
+			throw new IllegalArgumentException(msg == null ? String.format("Parameter array must have <%d> elements (<%d> provided)", size) : msg.get());
+		return arr;
+	}
+
+	private Validator() {
+	}
+
+}
