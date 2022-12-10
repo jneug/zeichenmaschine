97ea610f3414d28fb3fc423eb5e74def198ee9f2
J. Neugebauer <ngb@helmholtz-bi.de>
2022-12-10 11:14:38 +0100

    Dokumentation

--- src/main/java/schule/ngb/zm/util/events/Listener.java
+++ src/main/java/schule/ngb/zm/util/events/Listener.java
@@ -1,5 +1,11 @@
 package schule.ngb.zm.util.events;
 
+/**
+ * Generische Basisschnittstelle für die Implementierung von EventListenern.
+ *
+ * @param <E> Typ der Event-Objekte, die von diesem Listener empfangen werden.
+ * @see EventDispatcher
+ */
 public interface Listener<E> {
 
 }
